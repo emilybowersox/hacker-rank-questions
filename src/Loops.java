63c7c89e219b5bf54fce82f63aa5768b648f832e
Emily Bowersox <emily.bowersox100@gmail.com>
2020-08-14 10:41:54 -0500

    loop exercise

---
+++ src/Loops.java
@@ -0,0 +1,14 @@
+public class Loops {
+    public static void main(String[] args) {
+//        int N = scanner.nextInt();
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int i = 1;
+        int N = 2;
+        for (i=1; i<=10; i++){
+            System.out.println(N + " x " + i + " = " + N*i);
+        }
+
+//        scanner.close();
+    }
+
+}
