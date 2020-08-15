86784d2bfd9ef8c063db1153fba17dc253e2e7ef
Emily Bowersox <emily.bowersox100@gmail.com>
2020-08-15 17:33:14 -0500

    using long

--- src/LoopsTwo.java
+++ src/LoopsTwo.java
@@ -17,20 +17,33 @@ public class LoopsTwo {
 //CompareTriplets
 
     static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
-       int  alicePoints = 0;
-       int bobPoints = 0;
+        int alicePoints = 0;
+        int bobPoints = 0;
 
-       for (int i = 0; i <= 2; i++)
-       if (a.get(i) > b.get(i)) {
-           alicePoints += 1;
-       } else if (b.get(i) > a.get(i)) {
-           bobPoints += 1;
-       } else {
-           continue;
-       }
-       List<Integer> scores = new ArrayList<Integer>();
-       scores.add(alicePoints);
-       scores.add(bobPoints);
-       return scores;
+        for (int i = 0; i <= 2; i++)
+            if (a.get(i) > b.get(i)) {
+                alicePoints += 1;
+            } else if (b.get(i) > a.get(i)) {
+                bobPoints += 1;
+            } else {
+                continue;
+            }
+        List<Integer> scores = new ArrayList<Integer>();
+        scores.add(alicePoints);
+        scores.add(bobPoints);
+        return scores;
     }
+
+
+//aVeryBigSum
+
+    static long aVeryBigSum(long[] ar) {
+        long n = ar.length;
+        long sum = 0;
+        for (int i=0; i < n; i++) {
+            sum += ar[i];
+        }
+      return sum;
+    }
+
 }
