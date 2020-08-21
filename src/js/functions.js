9ed7eec2158641fc0b07b59de79d1f093bfbc0ed
Emily Bowersox <emily.bowersox100@gmail.com>
2020-08-20 22:12:00 -0500

    first case

--- src/js/functions.js
+++ src/js/functions.js
@@ -7,18 +7,36 @@
 //     }
 // }
 
-console.log(factorial(4));
-console.log("hello");
+// console.log(factorial(4));
+// console.log("hello");
+//
+//
+//
+// function factorial(aNumber) {
+//     var sum = 0;
+//     var n = 1
+//     do {
+//         n * (n-1);
+//         n++;
+//     } while (n <= aNumber);
+//
+// }
 
-
-
-function factorial(aNumber) {
-    var sum = 0;
-    var n = 1
-    do {
-        n * (n-1);
-        n++;
-    } while (n <= aNumber);
+function getGrade(score) {
+    if (25 < score <= 30) {
+        return "A";
+    } else if (20 < score <= 25) {
+        return "B";
+    } else if (15 < score <= 20) {
+        return "C";
+    } else if (10 < score <= 15) {
+        return "D";
+    } else if (5 < score <= 10) {
+        return "E";
+    } else {
+        return "F";
+    }
 
 }
 
+console.log(getGrade(15));
