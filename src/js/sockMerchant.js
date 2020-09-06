45430299104b57e409bef580d3968c97fbfd817e
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-05 19:54:51 -0500

    if-else with sortedArray.shift() - correct

--- src/js/sockMerchant.js
+++ src/js/sockMerchant.js
@@ -1,22 +1,36 @@
 "use strict";
 
 //var sortedArray = ar.sort();
-// modulus with ===
-
 
 function sockMerchant(n, ar) {
     var sum = 0;
     var sortedArray = ar.sort();
 
-    // for (var i = 0; i < n; i++){
-    //     if (sortedArray[i] == sortedArray[i+=1]) {
-    //         sum += 1;
-    //     }
-    // }
-    if (sortedArray[0] === sortedArray[7]){
-        sum += 1;
+    while (sortedArray.length > 1) {
+        if (sortedArray[0] === sortedArray[1]){
+            sum += 1;
+            sortedArray.shift();
+            sortedArray.shift();
+        } else {
+            sum += 0;
+            sortedArray.shift();
+        }
     }
-    // console.log(sortedArray[1]);
     return sum;
 }
 
+
+//removes first element of array (
+// .shift()
+
+//correct!!
+// while (sortedArray.length > 1) {
+//     if (sortedArray[0] === sortedArray[1]){
+//         sum += 1;
+//         sortedArray.shift();
+//         sortedArray.shift();
+//     } else {
+//         sum += 0;
+//         sortedArray.shift();
+//     }
+// }
