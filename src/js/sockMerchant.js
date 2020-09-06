52063669ba073e7d65cfb78809bee8361fb4f7c7
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-05 19:43:20 -0500

    === and sum

--- src/js/sockMerchant.js
+++ src/js/sockMerchant.js
@@ -11,3 +11,20 @@ function sockMerchant(n, ar) {
     console.log(ar[i]);
     return sum;
 }
+
+//tests
+function sockMerchant(n, ar) {
+    var sum = 0;
+
+    // for (var i = 0; i < n; i++){
+    //     if (ar[i] == ar[i+=1]) {
+    //         sum += 1;
+    //     }
+    // }
+    if (ar[1] === ar[2]){
+        sum += 1;
+    }
+    console.log(ar[1]);
+    return sum;
+}
+
