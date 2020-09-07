69e772d045032094c40f10f1f087bfd605790b31
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-07 15:19:47 -0500

    sea level

--- src/js/countingValleys.js
+++ src/js/countingValleys.js
@@ -1,6 +1,23 @@
 "use strict";
 
 function countingValleys(n, s) {
+
+    var seaLevel = 0;
+
+    for (var i = 0; i < n; i++) {
+        if (s[i] == "U") {
+            seaLevel += 1;
+        } else if (s[i] == "D") {
+            seaLevel -= 1;
+        }
+        return seaLevel;
+    }
+
+
+
+
+
+
     var up = 0;
 
     for (var i = 0; i < n; i++) {
