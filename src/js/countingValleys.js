3e6e469c538bec95512f47f28200409761a897cd
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-07 15:20:15 -0500

    return on the wrong line

--- src/js/countingValleys.js
+++ src/js/countingValleys.js
@@ -10,21 +10,18 @@ function countingValleys(n, s) {
         } else if (s[i] == "D") {
             seaLevel -= 1;
         }
-        return seaLevel;
     }
+    return seaLevel;
 
 
 
-
-
-
-    var up = 0;
-
-    for (var i = 0; i < n; i++) {
-        if (s[i] == s[i +1]) {
-            up += 1;
-        }
-    }
-    return up;
+    // var up = 0;
+    //
+    // for (var i = 0; i < n; i++) {
+    //     if (s[i] == s[i +1]) {
+    //         up += 1;
+    //     }
+    // }
+    // return up;
 
 }
