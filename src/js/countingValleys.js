b4d746c5b6c1de6d63179196ab0766ea2713f59f
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-07 15:35:03 -0500

    doesn't work in all cases

--- src/js/countingValleys.js
+++ src/js/countingValleys.js
@@ -1,17 +1,24 @@
 "use strict";
 
 function countingValleys(n, s) {
-
+    var valleyCount = 0;
     var seaLevel = 0;
 
     for (var i = 0; i < n; i++) {
         if (s[i] == "U") {
             seaLevel += 1;
+            if (seaLevel == 0) {
+                valleyCount += 1;
+            }
         } else if (s[i] == "D") {
             seaLevel -= 1;
+            if (seaLevel == 0) {
+                valleyCount += 1;
+            }
         }
     }
-    return seaLevel;
+
+    return valleyCount - 1;
 
 
     // var up = 0;
