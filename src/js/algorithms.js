1fcaa287f8d37ad929bdc687a6a3327bd721357a
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-11 15:36:34 -0500

    another approach with Math.max

--- src/js/algorithms.js
+++ src/js/algorithms.js
@@ -1,13 +1,27 @@
 "use strict";
 
+//this function doesn't work for test cases where there 99,999 "1000"s and 1 "999"
+// function birthdayCakeCandles(candles) {
+//     var sum = 1;
+//     var myString = candles.sort().reverse();
+//     var newArray = Array.prototype.map.call(myString, eachLetter => eachLetter);
+//     for (var i = 0; i < candles.length; i++) {
+//         if (newArray[i] === newArray[i+1]) {
+//             sum += 1;
+//         } else {
+//             break;
+//         }
+//     }
+//     return sum;
+// }
+
 function birthdayCakeCandles(candles) {
-    function birthdayCakeCandles(candles) {
-        var sum = 1;
-        var myString = candles.sort().reverse();
-        var newArray = Array.prototype.map.call(myString, eachLetter => eachLetter);
-
-        return newArray[0];
-
+    const max = Math.max(...candles);
+    var count = 0;
+    for (var i = 0; i < candles.length; i++) {
+        if (candles[i] === max) {
+            count++;
+        }
     }
-
+    return count;
 }
