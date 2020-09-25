f17de3fa711d4fbd6f4a9f413dce66642d9fe2c5
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-24 22:47:45 -0500

    works in some cases

--- src/js/tenDaysOfJS.js
+++ src/js/tenDaysOfJS.js
@@ -3,15 +3,11 @@
 //try, catch, finally
 function reverseString(s) {
     try {
-        var split = s.split("");
-        var reverse = split.reverse();
-        var join = reverse.join("");
-//   return s.split("").reverse().join("");
+    s = s.split("").reverse().join("");
     } catch (error) {
         console.error(error);
     } finally {
-        //made it a recursive loop oops
-        console.log(reverseString(s));
+        console.log(s);
     }
 
 }
