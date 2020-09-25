40610fcc75e06f4106a6a4d0096196abce3150b5
Emily Bowersox <emily.bowersox100@gmail.com>
2020-09-24 22:42:17 -0500

    split, reverse, join

--- src/js/tenDaysOfJS.js
+++ src/js/tenDaysOfJS.js
@@ -3,13 +3,14 @@
 //try, catch, finally
 function reverseString(s) {
     try {
-        var split = s.split(" ");
+        var split = s.split("");
         var reverse = split.reverse();
-
+        var join = reverse.join("");
+//   return s.split("").reverse().join("");
     } catch (error) {
         console.error(error);
     } finally {
-        console.log(split);
+        console.log(reverseString(s));
     }
 
 }
