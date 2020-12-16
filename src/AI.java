a595f2e62e67a7a5636668097789fd9b90c42d4b
Emily Bowersox <emily.bowersox100@gmail.com>
2020-12-15 18:23:48 -0600

    js 101 practice

---
+++ src/AI.java
@@ -0,0 +1,23 @@
+public class AI {
+
+    //Bot saves princess
+    //this isn't the right code, just notes
+//    public static void main(String[] args) {
+//
+//        if p = [0][0]{
+//            (n - 1) / 2 Up
+//                    (n - 1) / 2 Left
+//        } else if (p = [0][n]){
+//            (n - 1) / 2 Left
+//                    (n - 1) / 2 down
+//        } else if (p = [n][0]){
+//            (n - 1) / 2 right
+//                    (n - 1) / 2 up
+//        } else if (p = [n][n]){
+//            (n - 1) / 2 right
+//                    (n - 1) / 2 down
+//        }
+//    }
+//
+
+}
---
+++ src/js/jsDrills.js
@@ -0,0 +1,5 @@
+// 43 Write a function definition named isVowel that takes in value and returns true if the value is a, e, i, o, u in upper or lower case.
+function isVowel(x) {
+    return x === "a" || x === "e" || x === "i" || x === "o" || x == "u" || x === "A" || x === "E" || x === "I" || x === "O" || x === "U";
+}
+
