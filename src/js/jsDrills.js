5271a87ce451954860b89b285a581a7f346f0c69
Emily Bowersox <emily.bowersox100@gmail.com>
2020-12-15 18:24:41 -0600

    function hasVowels

--- src/js/jsDrills.js
+++ src/js/jsDrills.js
@@ -3,3 +3,15 @@ function isVowel(x) {
     return x === "a" || x === "e" || x === "i" || x === "o" || x == "u" || x === "A" || x === "E" || x === "I" || x === "O" || x === "U";
 }
 
+// 44 Write a function definition named hasVowels that takes in value and returns true if the string contains any vowels.
+var vowels = ["a", "e", "i", "o", "u"];
+
+function hasVowels (aString){
+    for (var i = 0; i <vowels.length; i++){
+        if (aString.includes(vowels[i])){
+            return true;
+        }
+    }
+    return false;
+}
+
